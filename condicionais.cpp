358a1a6f2d133203c252c3306578db966e20ca0e
Marcio Barbosa <mmsb21@gmail.com>
2022-09-06 23:35:34 -0300

    condicionais

--- condicionais.cpp
+++ condicionais.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-<<<<<<< HEAD
 
 int main()
 {
@@ -83,37 +82,3 @@ int main()
         cout << "uma linha";
     return 0;
 }
-=======
-    
-int main(){
-    int x= 10;
-
-    cout<< "O valor é igual a 10: "<<(x==10)<<endl;
-    cout<< "O valor é maior que 10: "<<(x>10)<<endl;
-    cout<< "O valor é menor que 10: "<<(x<10)<<endl;
-    cout<< "O valor é maior ou igual a 10: "<<(x>=10)<<endl;
-    cout<< "O valor é menor ou igual a 10: "<<(x<=10)<<endl;
-    cout<< "O valor é diferente de 10: "<<(x!=10)<<endl;
-
-    bool boole1 = (x>10) ? true:false;
-    bool boole2 = (x<10) ? true:false;
-    bool boole3 = (x==10) ? true:false;
-    bool boole4 = (x!=10) ? true:false;
-    cout << boole1<< endl;
-    cout << boole2<< endl;
-    cout << boole3<< endl;
-    cout << boole4<< endl;
-
-    string resultado1 = (x>10) ? "Verdadeiro":"Falso";
-    string resultado2 = (x<10) ? "Verdadeiro":"Falso";
-    string resultado3 = (x==10) ? "Verdadeiro":"Falso";
-    string resultado4 = (x!=10) ? "Verdadeiro":"Falso";
-
-    cout << resultado1<< endl;
-    cout << resultado2<< endl;
-    cout << resultado3<< endl;
-    cout << resultado4<< endl;
-
-    return 0;
-    }
->>>>>>> bfc088da45967a0582d9dc2d99a3e969b0aeecdf
